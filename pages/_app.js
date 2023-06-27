d575c8c418bf86c5e456352c56eabd6841980ead
subrota <subrota45278@gmail.com>
2023-06-27 21:21:02 +0600

    add commit for app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import Head from 'next/head'
 import Layout from '../Components/Layouts/Layouts'
 import '../styles/globals.css'
 import React from 'react'
+//aos for scrolling animation
 import Aos from 'aos';
+//react photo view
 import 'react-photo-view/dist/react-photo-view.css';
 function MyApp({ Component, pageProps }) {
 //aos scroll animation
@@ -15,8 +17,10 @@ Aos.init({
 
   return <>
   <Head>
+  {/* logo image */}
   <link rel="shortcut icon" href="https://i.ibb.co/LSD1zwg/product-logo.png" type="image/x-icon" />
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
+ {/* Aos cdn link  */}
+ <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   </Head>
   <div id="mainDiv">
